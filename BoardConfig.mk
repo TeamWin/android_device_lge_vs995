14c32159971be25ff067536ea19a943717f816ad
Phoenix591 <11486+Phoenix591@users.noreply.github.com>
2018-02-06 04:03:08 +0000

    Added TW_CLOCK  offset flag, potentially H918 has different offset
    Change-Id: I7f79ae64df0ff0cdead86a5172f3e1db817979d5

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -77,6 +77,11 @@ TW_IGNORE_MISC_WIPE_DATA := true
 #Bottom
 #TW_H_OFFSET := -35
 
+TW_CLOCK_OFFSET := -1451606400
+#US996 offset 46 years
+#H918 TW_CLOCK_OFFSET := -1385672400
+
+
 # exFAT drivers included in the kernel
 TW_NO_EXFAT_FUSE := true
 
