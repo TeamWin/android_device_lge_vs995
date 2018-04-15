bd910eff56e0be9364d31b090c71325613265509
Phoenix591 <11486+Phoenix591@users.noreply.github.com>
2018-04-15 01:16:50 -0500

    date was.now off by one, fixing

--- recovery/root/fixdate.sh
+++ recovery/root/fixdate.sh
@@ -10,6 +10,20 @@ done
 sleep 2
 #so now I have lineage 15.1 giving a year of 2019 with new kernel. sick of playing wackamole so forcing year to 2018 and will update for year end later
 export ryear=2018
+#day is now one off
+export day=$(expr $(date +%j) - 1)
+#now handle end of month
+if [ $day -eq 0 ]; then
+	export month=$(expr $(date +%m) -1)
+	case $month
+		01|03|05|07|08|10|12) export day=31;;
+		02) export day=28;;
+		04|06|09|11) export day=30;;
+		*) export day=31;; #shouldnt get here
+	esac
+#doing full adjustment hereand not below 
+	date $month$day$(date +%H%M)$ryear.$(date +%S)
+fi
 #export year=`date +%Y`
 #if [ $year -lt 2000 ]
 #then
@@ -19,7 +33,9 @@ export ryear=2018
 #On superv20 us996 10o I had it come up 2017 so handling this case
 if [ `date +%Y`  -ne $ryear ]
 then
-date $(date +%m%d%H%M)2018.$(date +%S)
+export time=$(date +%H%M)
+export second=$(date +%S)
+date $(date +%m)$day$time$ryear.$second
 #echo "Did 2017 date nudge" >>/twrp-date.log
 fi
 sleep 4
