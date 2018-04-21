67aad8c02b96d81629b4f7dc1fae87fc851cdf17
Phoenix591 <11486+Phoenix591@users.noreply.github.com>
2018-04-21 00:38:58 -0500

    Oreo encryotion fixed

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -85,7 +85,7 @@ TW_EXCLUDE_SUPERSU := true
 # Encryption support
 TW_INCLUDE_CRYPTO := true
 # needed for Lineage encryption
-TW_CRYPTO_USE_SYSTEM_VOLD := qseecomd
+TW_CRYPTO_USE_SYSTEM_VOLD := true
 TARGET_HW_DISK_ENCRYPTION := true
 TARGET_KEYMASTER_WAIT_FOR_QSEE := true
 
