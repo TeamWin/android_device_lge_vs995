b2b05020511c5b1827954f80eb2110e7f9015ecd
Phoenix591 <11486+Phoenix591@users.noreply.github.com>
2018-07-25 16:17:38 -0500

    rework timescript, still a bit wip but is less hacky than previous

--- recovery/root/fixdate.sh
+++ recovery/root/fixdate.sh
@@ -8,46 +8,33 @@ do
     sleep 1
 done
 sleep 2
-while true ; do
-#so now I have lineage 15.1 giving a year of 2019 with new kernel. sick of playing wackamole so forcing year to 2018 and will update for year end later
-export ryear=2018
-#day is now one off
-export day=$(expr $(date +%d) - 1)
-#date needs single digit days and months to have a leading zero
-if [ $day -lt 10 ]; then
-	export day=0$day
-fi
 
-
-#now handle end of month
-if [ $day -eq 00 ]; then
-	export month=$(expr $(date +%m) -1)
-	if [ $month -lt 10 ]; then
-		export month=0$month
+export year=`date +%Y`
+export checked=$1
+if [ -z "$checked"]; then
+#check if a stock rom and set a flag so it wont recheck on the same boot
+	mount /system -o ro
+	grep lge.swversion /system/build.prop
+	if (( $? != 0 )); then
+		exit
 	fi
-
-	case $month in
-		01|03|05|07|08|10|12) export day=31;;
-		02) export day=28;;
-		04|06|09|11) export day=30;;
-		*) export day=31;; #shouldnt get here
-	esac
-#doing full adjustment hereand not below
-	date $month$day$(date +%H%M)$ryear.$(date +%S)
+	umount /system
+	export checked=1
 fi
-#export year=`date +%Y`
-#if [ $year -lt 2000 ]
+export year=`expr $year + 46`
+if [[ $year -ge 2030 ]]; then
+	exit
+fi
+date $(date +%m%d%H%M)$year.$(date +%S)
+fi
+##On superv20 us996 10o I had it come up 2017 so handling this case
+#if [ `date +%Y` -eq 2017 ]
 #then
-#export year=`expr $year + 46`
-#date $(date +%m%d%H%M)$year.$(date +%S)
-#fi
-#On superv20 us996 10o I had it come up 2017 so handling this case
-if [ `date +%Y`  -ne $ryear ]
-then
-export time=$(date +%H%M)
-export second=$(date +%S)
-date $(date +%m)$day$time$ryear.$second
+#date $(date +%m%d%H%M)2018.$(date +%S)
 #echo "Did 2017 date nudge" >>/twrp-date.log
 fi
-sleep 0.3
-done
+sleep 4
+# Now not sure about the next comment, so being more aggressive and checking every 6 seconds.
+#date gets switched back to 1972 20 seconds after twrp starts so waiting 19 seconds + 2 seconds to fix on the next run
+exec /fixdate.sh $checked
+
