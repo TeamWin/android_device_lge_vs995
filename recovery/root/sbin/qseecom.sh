abeef1e4773db123d5bae4bb0e8b50bddc99ea02
Phoenix591 <11486+Phoenix591@users.noreply.github.com>
2018-08-25 03:08:20 -0500

    Initial test of system as root decryption
    Use mountpoint to check if /data and /system are mounted
    If system as root (checked by existence of /system/system)
    bind mount /system/system to /gsi and
    start vold and qseecomd from there
    
    Change-Id: Id3e96878792e6bd4e5f08f4ae91f4342a0f6c579

--- recovery/root/sbin/qseecom.sh
+++ recovery/root/sbin/qseecom.sh
@@ -1,6 +1,20 @@
 #!/sbin/sh
-while [ "$(getprop ro.crypto.fs_crypto_blkdev)" != "/dev/block/dm-0" ]; do
-	if [ ! -d /system/etc ]; then #mount system if not already mounted
+if !  mountpoint /system; then
+	mount -o ro /system
+fi
+if [ -d /system/system ]; then
+	export GSI=true
+	mkdir /gsi
+	mount --bind /system/system /gsi
+fi
+
+while ! mountpoint  /data; do
+	if [ -z "$GSI" ]; then
+		export system=/system
+	else #if system as root treat /system/system (bind mounted at /gsi) as /system
+		export system=/gsi
+	fi
+	if ! mountpoint /system; then #mount system if not already mounted
       		mount /system -o ro
 	fi
         if blkid /dev/block/bootdevice/by-name/vendor | grep ext4; then
@@ -16,28 +30,29 @@ while [ "$(getprop ro.crypto.fs_crypto_blkdev)" != "/dev/block/dm-0" ]; do
 			rmdir /vendor
 		fi
 		if [ ! -L /vendor ]; then
-        		ln -s /system/vendor /vendor #if no vendor partition symlink it
+        		ln -s $system/vendor /vendor #if no vendor partition symlink it
+
 		fi
         fi
-        if [ -f /system/bin/qseecomd ]; then
+        if [ -f $system/bin/qseecomd ]; then
 #                start sys_qseecomd
 		if [ -z "$(pidof qseecomd)" ]; then #make sure it isnt already running
-			LD_LIBRARY_PATH='/system/lib64:/system/lib' PATH='/system/bin' /system/bin/qseecomd &
+			LD_LIBRARY_PATH='$system/lib64:$system/lib' PATH='$system/bin' $system/bin/qseecomd &
 		fi
         else
 #                start ven_qseecomd
 		if [ -z "$(pidof qseecomd)" ]; then
-			LD_LIBRARY_PATH='/vendor/lib64:/system/lib64:/vendor/lib:/system/lib' PATH='/vendor/bin:/system/bin' /vendor/bin/qseecomd &
+			LD_LIBRARY_PATH='/vendor/lib64:$system/lib64:/vendor/lib:$system/lib' PATH='/vendor/bin:$system/bin' /vendor/bin/qseecomd &
 		fi
         fi
-#	while [ ! -d /data/media ]; do
-#		sleep 0.02
-#		if [ ! -d /system/etc ]; then #sometimes /system gets unmounted
-#			mount /system -o ro
-#		fi
-#	done
-#	exec /sbin/sh /sbin/qseecom.sh
+	if [ ! -z "$GSI" ]; then
+		if [ -z "(pidof vold)" ]; then
+			start gsi_vold
+		fi
+	fi
 done
-if [ -d /system/etc ]; then
-#	umount /system
+#data now mounted. Clean up
+if [ ! -z "$GSI" ]; then
+	umount /gsi
 fi
+umount /system
